9ff704cfcde536115ec195973652984f79a7868e
quanweiwang <629526514@qq.com>
2018-12-03 22:51:13 +0800

    11
    11

--- BiLing/ViewController.m
+++ BiLing/ViewController.m
@@ -7,9 +7,19 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
+
+//AutoLayout
+//define this constant if you want to use Masonry without the 'mas_' prefix
+#define MAS_SHORTHAND
+
+//define this constant if you want to enable auto-boxing for default syntax
+#define MAS_SHORTHAND_GLOBALS
+
+#import "Masonry.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) WKWebView * webView;
 @end
 
 @implementation ViewController
@@ -17,6 +27,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //初始化UI
+    [self initUI];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self.webView evaluateJavaScript:@"deviceInfo('8.3')" completionHandler:^(id _Nullable result, NSError * _Nullable error) {
+        NSLog(@"ok");
+    }];
+}
+
+- (void)initUI {
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.bl-coin.cn/tpl/index.html"]];
+    WKWebView *webView = [[WKWebView alloc] initWithFrame:CGRectZero];
+    [webView loadRequest:request];
+    
+    [self.view addSubview:webView];
+    self.webView = webView;
+    
+    //layout
+    [webView makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
 }
 
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+target "BiLing" do
+	
+	platform :ios, '8.0'
+	use_frameworks!
+	inhibit_all_warnings!
+
+	pod 'Masonry', '~> 1.1.0'
+
+end
+
