f6b006bc1c89748d3bf40a7c2b819799321be5fc
quanweiwang <629526514@qq.com>
2018-12-03 20:07:18 +0800

    Initial Commit

---
+++ BiLing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BiLing
+//
+//  Created by 王权伟 on 2018/12/3.
+//  Copyright © 2018 BiLing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
